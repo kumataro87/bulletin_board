8b46e62bfc8395b30be91c00e6f0451afc1631cc
kumataro87 <371491s5@gmail.com>
2022-08-28 22:12:23 +0900

    Change users edit view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root 'static_pages#index'
 end
